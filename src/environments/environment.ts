7895ed06ea45f5b2e4508fc240ad7ad6e3ae5e8a
Anatoliy Sysoienko <pistalierro@gmail.com>
2026-04-03 17:25:08 +0300

    chore(firebase): configure Firebase app providers and environment setup

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDMKlKZfWpZMPepKDoYgqyh7CEjnauI5Vg',
+    authDomain: 'inventoryops-85d57.firebaseapp.com',
+    projectId: 'inventoryops-85d57',
+    storageBucket: 'inventoryops-85d57.firebasestorage.app',
+    messagingSenderId: '416114756001',
+    appId: '1:416114756001:web:a447f2a613da68739423e0'
+  }
+};
