79b1e003afd50d3adc77f1af4b106c156d3c17dd
Anatoliy Sysoienko <pistalierro@gmail.com>
2026-04-04 13:52:25 +0300

    feat(app): scaffold feature page route components

---
+++ src/app/features/products/pages/product-edit-page/product-edit-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-edit-page',
+  imports: [],
+  templateUrl: './product-edit-page.html',
+  styleUrl: './product-edit-page.scss',
+})
+export class ProductEditPage {
+
+}
